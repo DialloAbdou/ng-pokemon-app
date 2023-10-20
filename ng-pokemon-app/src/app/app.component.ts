ada637a0982e65164b2f04aa2addaaf4c3742abd
DialloAbdou <=dialloabdou@hotmail.com>
2023-10-20 11:48:32 +0200

    Amélioration du composant racine

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {POKEMONS} from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
+      <h1>Liste des Pokemons!</h1>
     </div>
- 
   `,
-
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonList : Pokemon[]=POKEMONS ;
+  ngOnInit() {
+     
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(` vous avez selectionnez ${pokemon.name}`);
+  }
 }
